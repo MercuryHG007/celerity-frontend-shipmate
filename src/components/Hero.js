4bc9cbddd091954454308ac91b51ae00ab5df898
Harshit Garg <ggharshit1@gmail.com>
2023-04-27 14:28:25 +0530

    HeroSection Added

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+
+function Hero() {
+  return (
+    <div
+        className="flex flex-col justify-center items-center mt-[12vh] pt-16"
+    >
+        <div
+            className="text-4xl font-bold"
+        >
+            Hassle-Free Shipping Solutions
+        </div>
+        <div
+            className="text-lg font-semibold"
+        >
+            Compare, book, and manage your freight across the world&apos;s top logistics providers, all on one platform.
+        </div>
+    </div>
+  )
+}
+
+export default Hero
